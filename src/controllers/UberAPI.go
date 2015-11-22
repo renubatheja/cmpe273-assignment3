d3824e33bc9e2df0355fdef8f624e8706f8ec838
rbatheja <rbatheja@drishticon.com>
2015-11-22 09:08:59 -0800

    CMPE_273-Assignment 3 Submission (A Location and Trip Planner Service Part ||)

--- src/controllers/UberAPI.go
+++ src/controllers/UberAPI.go
@@ -17,8 +17,18 @@ type Response struct {
 
 type PricesStruct struct {
 	LowEstimate float64 `json:"low_estimate"`
+	HighEstimate float64 `json:"high_estimate"`
 	Duration float64 `json:"duration"`
 	Distance float64 `json:"distance"`
+}
+
+type ProductTypeResponse struct {
+	Results []ProductTypesStruct `json:"products"`
+}
+
+type ProductTypesStruct struct {
+	DisplayName string `json:"display_name"`
+	ProductID string `json:"product_id"`
 }
 
 var Client *uber.Client
@@ -97,9 +107,9 @@ func CallUberAPIForPriceEstimates(start_latitude float64, start_longitude float6
 		//find lowest uber cost and then lowest uber duration
 		minUberCost = 999999999
 		for index := 0; index < len(response.Results); index++ {
-	 		uberCost = response.Results[index].LowEstimate
+	 		uberCost = response.Results[index].HighEstimate
 	 		//fmt.Println("uberCost : ",uberCost)
-	 		if(uberCost < minUberCost) {
+	 		if(uberCost < minUberCost && uberCost != 0) {
 	 			minUberCost = uberCost
 		 		uberDuration = response.Results[index].Duration
 		 		uberDistance = response.Results[index].Distance		
@@ -122,6 +132,57 @@ func CallUberAPIForPriceEstimates(start_latitude float64, start_longitude float6
 }
 
 
+
+/*
+* Function to make Uber API Product type call to get the Product ID
+*/
+var ProductID string
+
+func CallUberAPIForProductType(start_latitude float64, start_longitude float64) string {
+	ProductID = ""
+	var query string
+	
+	startLatitude := strconv.FormatFloat(start_latitude, 'f', -1, 32)
+	startLongitude := strconv.FormatFloat(start_longitude, 'f', -1, 32)
+	query = "https://api.uber.com/v1/products?latitude=" + string(startLatitude) + "&longitude=" + string(startLongitude) + "&server_token=XuKMSJ2-MqcHPOT2XXKJChZYGcXkyNJgYG2LOSQj"
+	resp, err := http.Get(query)
+	
+	if err != nil {
+		fmt.Println("Error : Error received while executing Uber's Price Estimates API call! : ", err);
+		fmt.Println("------------------------------------------------------------")
+	}
+	
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Error : Error received while reading response from Uber's Price Estimates API call! : ", err);
+		fmt.Println("------------------------------------------------------------")	
+	}
+		
+	var response ProductTypeResponse
+	errUnmarshal := json.Unmarshal(body, &response)
+	if errUnmarshal != nil {
+		fmt.Println("Error : Error received while unmashalling response from Uber's Price Estimates API call!: ", errUnmarshal);
+		fmt.Println("------------------------------------------------------------")	
+	}
+		
+	if(len(response.Results) > 0){
+		for index := 0; index < len(response.Results); index++ {
+			if(response.Results[index].DisplayName == "uberX") {
+				ProductID = response.Results[index].ProductID
+				break
+			} 
+		}
+	} else {
+		fmt.Println("Error : No results returned! Please enter a valid location")
+		fmt.Println("------------------------------------------------------------")
+	}
+	fmt.Println("Product ID : " + ProductID)
+	return ProductID
+}
+
+
 /*
 * Function to make Uber API Ride Request call
 */
@@ -133,7 +194,9 @@ func CallUberAPIForRideRequest(lc TripController, StartingFromLocationId string,
 	  rideResponse := model.RideRequestResponse{}
 	  	  	  
 	  fmt.Println("Let's see how much time it would take for driver to reach here!")
-	  _, err, responseRide := Client.PostRequest("2832a1f5-cfc0-48bb-ab76-7ea7a62060e7",startLatitude,startLongitude,destinationLatitude,destinationLongitude,"")
+	  ProductID := CallUberAPIForProductType(startLatitude, startLongitude)
+	  //_, err, responseRide := Client.PostRequest("2832a1f5-cfc0-48bb-ab76-7ea7a62060e7",startLatitude,startLongitude,destinationLatitude,destinationLongitude,"")
+	  _, err, responseRide := Client.PostRequest(ProductID,startLatitude,startLongitude,destinationLatitude,destinationLongitude,"")
 	  //requestRide, err := client.PostRequest("a1111c8c-c720-46c3-8534-2fcdd730040d",37.355,-122.0,37.38,-122.01,"")
 	  if (err != nil && err.Error() != "EOF") {
 		fmt.Println("Error Occurred while Posting a ride request: " , err)
